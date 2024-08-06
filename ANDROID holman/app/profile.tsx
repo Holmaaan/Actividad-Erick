4c58be001896c4671f84ed70b900aaca604f5797
Holmaaan <159957351+Holmaaan@users.noreply.github.com>
2024-08-06 14:19:42 -0500

    Add files via upload

---
+++ ANDROID holman/app/profile.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const Profile = () => {
+    return (
+        <View> 
+
+            <Text>Inicio</Text>
+        </View>
+    )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
